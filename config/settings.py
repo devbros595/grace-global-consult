fb0c4fe75b62cfd9e1ff2ef805980950d4b29219
Elliot Achugbue <atejiri@outlook.com>
2026-06-11 18:44:08 +0100

    feat: integrate Cloudinary for media storage and update requirements

--- config/settings.py
+++ config/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary",
+    "cloudinary_storage",
     "core",
 ]
 
@@ -151,5 +153,12 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+import cloudinary
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": "dkean9geh",
+    "API_KEY": "333477243859878",
+    "API_SECRET": "u2418_wNRV3E3kg7Zpk_P9yCkWY",
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 handler404 = "core.views.custom_404_view"
 
@@ -26,5 +24,3 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
-# FORCE media serving on Render
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
